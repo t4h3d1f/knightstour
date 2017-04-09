c8ca2293d3846050a3bd3de6cbee1cc42826da5b
t4h3d1f <thoomasallen8@gmail.com>
2017-04-08 19:15:56 -0700

    fix formatting

--- KnightsTour/KnightsTour.cpp
+++ KnightsTour/KnightsTour.cpp
@@ -2,11 +2,12 @@
 // Author:		Thoomas Allen
 // Section:		S
 // Assignment:	1
-// Description:	Knight's Tour finds a path for a knight chess piece to follow. The path takes place on a
-// standard chess board (can be changed to run on a larger or smaller board). The path leads the 
-// knight around the board, landing on every square once and only once. When the program 
-// finishes running it will display the first path it found that met the criteria, and how many 
-// paths the program tried before finding it. 
+// Description:	Knight's Tour finds a path for a knight chess piece to follow. 
+// The path takes place on a standard chess board 
+// (can be changed to run on a larger or smaller board). The path leads the 
+// knight around the board, landing on every square once and only once. 
+// When the program finishes running it will display the first path it found 
+// that met the criteria, and how many paths the program tried before finding it. 
 ////
 
 #include "stdafx.h"
@@ -19,8 +20,9 @@ int finalTile = 0;
 int board[BOARDSIZE][BOARDSIZE] = { 0 };
 long long failedAttempts = 0;
 
-//Prints the current board. shows numbers where the knight has been and zeros where it has not. 
-//the number shown corrisponds to which turn the knight was on when it landed on that spot.
+//Prints the current board. shows numbers where the knight has been and 
+//zeros where it has not. the number shown corrisponds to which turn the 
+//knight was on when it landed on that spot.
 void drawBoard()
 {
 	for (int i = 0; i < BOARDSIZE; i++)
@@ -33,9 +35,11 @@ void drawBoard()
 	}
 }
 
+//determines if the next move is legal.
 bool checkMove(int row, int col)
 {
-	if (!((0 <= row) && (row <= BOARDSIZE - 1) && (0 <= col) && (col <= BOARDSIZE - 1)))
+	if (!((0 <= row) && (row <= BOARDSIZE - 1) && 
+		(0 <= col) && (col <= BOARDSIZE - 1)))
 		return false;
 	if (!(board[row][col] == 0))
 		return false;
@@ -45,7 +49,8 @@ bool checkMove(int row, int col)
 //moveKnight checks if the knight is on the board, 
 //and if the space it is on has already been used in the current attempt.
 //if the prior checks clear, check if the board has been filled.
-//if the board has not been filled in completely, start trying to move the knight to a surrounding tile.
+//if the board has not been filled in completely, start trying to move 
+//the knight to a surrounding tile.
 bool moveKnight(int row, int col, int moveNum)
 {
 	if (moveNum == finalTile)
@@ -56,9 +61,11 @@ bool moveKnight(int row, int col, int moveNum)
 	if (failedAttempts % 10000000 == 0)
 	{
 		drawBoard();
-		failedAttempts++; //add one for every ten million failed attempt, since the program 
-						//only evaluates a failed attempt once it has run out of moves. 
-		cout << endl; //this would cause the board to be printed multiple times otherwise
+		failedAttempts++; 
+		//add one for every ten million failed attempt, since the program 
+		//only evaluates a failed attempt once it has run out of moves. 
+		//this would cause the board to be printed multiple times otherwise
+		cout << endl; 
 	}
 	board[row][col] = moveNum;
 	if (checkMove(row - 2, col + 1))
@@ -108,7 +115,8 @@ bool moveKnight(int row, int col, int moveNum)
 }
 
 
-//Ask the user where to place the knight on the board. Show the ending board and print out how many tries it took.
+//Ask the user where to place the knight on the board. 
+//Show the ending board and print out how many tries it took.
 int main()
 {
 	finalTile = BOARDSIZE*BOARDSIZE;
@@ -126,7 +134,8 @@ int main()
 	moveKnight(row, col, 1);
 	cout << "Solution found!\n";
 	drawBoard();
-	cout << "There were " << failedAttempts << " failed attempts to find a solution.\n";
+	cout << "There were " << failedAttempts << 
+		" failed attempts to find a solution.\n";
 	system("pause");
 	return 0;
 }
